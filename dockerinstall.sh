78ff12aa63742865d3b19f23bbb3affe65786b9c
srijb <32200340+srijb@users.noreply.github.com>
2018-12-22 22:33:12 -0500

    112233

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -14,7 +14,7 @@ ssh -o StrictHostKeyChecking=no \
        -p 22 ansible@$con \
 "sudo docker run -d --name dd-agent$i -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY=3d2a93340bd533472a1d394d348d1e52 datadog/agent:latest"
 done
-i--
+i=i-1
 done
 else
 echo "nothing to do"
