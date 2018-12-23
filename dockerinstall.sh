8fcf97f2d27e606473862f96205ce62a83bc853f
srijb <32200340+srijb@users.noreply.github.com>
2018-12-22 23:21:54 -0500

    76203743

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -7,14 +7,14 @@ then
 #read arg1
 #i=${arg1}
 #echo "Installing containers $i"
-i=3
-for a in $i; do
+i=1
+while [ $i <= 3 ] do
 for con in $(cat /home/ansible/ds/ddocker/container.txt); do
 ssh -o StrictHostKeyChecking=no \
        -p 22 ansible@$con \
 "sudo docker run -d --name dd-agent$i -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY=3d2a93340bd533472a1d394d348d1e52 datadog/agent:latest"
 done
-i=i-1
+i=$[$i+1]
 done
 else
 echo "nothing to do"
