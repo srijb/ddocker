3547d94e309ca7dacfc3c38dc0186becfb6b071b
srijb <32200340+srijb@users.noreply.github.com>
2018-12-28 11:51:14 -0500

    661521

---
+++ container.sh
@@ -0,0 +1,1 @@
+sudo docker run -d --name dd-agent$i -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY=3d2a93340bd533472a1d394d348d1e52 datadog/agent:latest
